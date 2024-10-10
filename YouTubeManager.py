2a92919b93e9581f2566d4bdf3a2ee72be1e26d4
Donkrzawayan <kadon991@gmail.com>
2024-10-10 21:49:03 +0200

    working playing module

---
+++ YouTubeManager.py
@@ -0,0 +1,18 @@
+import yt_dlp
+
+
+def extract_audio_url(query: str):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'noplaylist': True,
+        'quiet': True,
+    }
+
+    if not query.startswith('http'):
+        query = f"ytsearch:{query}"
+    
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(query, download=False)
+        if 'entries' in info:
+            info = info['entries'][0]
+        return info['url'], info['webpage_url']
--- app.py
+++ app.py
@@ -1,23 +1,76 @@
 import discord
+from discord.ext import commands
 
+from YouTubeManager import extract_audio_url
 from config import DISCORD_TOKEN
 
 intents = discord.Intents.default()
 intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
 
-client = discord.Client(intents=intents)
+FFMPEG_OPTIONS = {
+    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+    'options': '-vn'
+}
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'Bot is ready. Logged in as {bot.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
+@bot.command(name='play', help='Plays a song from YouTube. If a search query is given, it plays the first result.')
+async def play(ctx, *, url):
+    voice_channel = ctx.author.voice.channel
+
+    if not voice_channel:
+        await ctx.send("You're not connected to a voice channel!")
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    if not voice_client:
+        voice_client = await voice_channel.connect()
 
-if __name__ == '__main__':
-    client.run(DISCORD_TOKEN)
+    if voice_client.is_playing():
+        voice_client.stop()
+
+    audio_url, source_url = extract_audio_url(url)
+    voice_client.play(discord.FFmpegPCMAudio(audio_url, **FFMPEG_OPTIONS))
+    await ctx.send(f'Now playing: {source_url}')
+
+@bot.command(name='leave', help='Makes the bot leave the voice channel.')
+async def leave(ctx):
+    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    if voice_client.is_connected():
+        await voice_client.disconnect()
+        await ctx.send("Disconnected from the voice channel.")
+    else:
+        await ctx.send("I'm not in a voice channel.")
+
+@bot.command(name='pause', help='Pauses the current song.')
+async def pause(ctx):
+    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    if voice_client.is_playing():
+        voice_client.pause()
+        await ctx.send("Music paused.")
+    else:
+        await ctx.send("No music is playing right now.")
+
+@bot.command(name='resume', help='Resumes the paused song.')
+async def resume(ctx):
+    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    if voice_client.is_paused():
+        voice_client.resume()
+        await ctx.send("Music resumed.")
+    else:
+        await ctx.send("No music is paused right now.")
+
+@bot.command(name='stop', help='Stops the current song.')
+async def stop(ctx):
+    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    if voice_client.is_playing():
+        voice_client.stop()
+        await ctx.send("Music stopped.")
+    else:
+        await ctx.send("No music is playing right now.")
+
+
+bot.run(DISCORD_TOKEN)
