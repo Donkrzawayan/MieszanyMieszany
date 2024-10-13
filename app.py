9eec799abf5528ae5f7b10d631a26cb6461242c9
Donkrzawayan <kadon991@gmail.com>
2024-10-13 23:17:34 +0200

    commands consistency; little refactor

--- app.py
+++ app.py
@@ -38,11 +38,7 @@ class MusicBot(commands.Cog):
             self.stats.increment_song_counter(guild_id)
             await ctx.send(f"Now playing: {source_url}")
 
-    @commands.check
-    async def only_allowed_channels(self, ctx):
-        return ctx.channel.id in ALLOWED_CHANNELS
-
-    @commands.command(name="play", help="Plays a song from YouTube. If search query - plays the first result.")
+    @commands.command(name="play", help="Plays a song from YouTube. If search query - plays the first result")
     async def play(self, ctx, *, query: str):
         if ctx.voice_client is None:
             if ctx.author.voice:
@@ -62,7 +58,7 @@ class MusicBot(commands.Cog):
         else:
             await self.play_next(ctx)
 
-    @commands.command(name="leave", help="Makes the bot leave the voice channel.")
+    @commands.command(name="leave", help="Makes the bot leave the voice channel")
     async def leave(self, ctx):
         if ctx.voice_client is not None:
             guild_id = ctx.guild.id
@@ -70,13 +66,13 @@ class MusicBot(commands.Cog):
             await ctx.voice_client.disconnect()
             await ctx.send("Disconnected from the voice channel.")
 
-    @commands.command(name="skip", help="Skips current song.")
+    @commands.command(name="skip", help="Skips current song")
     async def skip(self, ctx):
         if ctx.voice_client.is_playing():
             await ctx.send("Skipped the current song.")
             ctx.voice_client.stop()  # trigger play_next via after callback
 
-    @commands.command(name="queue", help="Lists current queue.")
+    @commands.command(name="queue", help="Lists current queue")
     async def queue(self, ctx):
         guild_id = ctx.guild.id
         if guild_id in self.song_queue and self.song_queue[guild_id]:
@@ -85,34 +81,36 @@ class MusicBot(commands.Cog):
         else:
             await ctx.send("The queue is empty.")
 
-    @commands.command(name="clear", help="Clear the queue.")
-    async def clear(self, ctx, guild_id=None):
-        if guild_id is None:
-            guild_id = ctx.guild.id
+    @commands.command(name="clear", help="Clears the queue")
+    async def clear(self, ctx):
+        guild_id = ctx.guild.id
         if guild_id in self.song_queue:
             self.song_queue[guild_id].clear()
             await ctx.send("Cleared the queue.")
 
-    @commands.command(name="stop", help="Stops the current song.")
+    @commands.command(name="stop", help="Stops the current song")
     async def stop(self, ctx):
         guild_id = ctx.guild.id
         if ctx.voice_client is not None:
             self.song_queue[guild_id].clear()
             ctx.voice_client.stop()
-            await ctx.send("Music stopped and queue cleared.")
+            await ctx.send("Music stopped and queue cleared")
 
     @tasks.loop(minutes=1)
     async def check_inactivity(self):
         for guild in self.bot.guilds:
-            if guild.voice_client and not guild.voice_client.is_playing():
-                if guild.id not in self.song_queue or not self.song_queue[guild.id]:
-                    await asyncio.sleep(DISCONNECT_AFTER)
-                    if guild.voice_client and not guild.voice_client.is_playing():
-                        await guild.voice_client.disconnect()
+            if (
+                guild.voice_client
+                and not guild.voice_client.is_playing()
+                and (guild.id not in self.song_queue or not self.song_queue[guild.id])
+            ):
+                await asyncio.sleep(DISCONNECT_AFTER)
+                if guild.voice_client and not guild.voice_client.is_playing():
+                    await guild.voice_client.disconnect()
 
-                        last_channel = self.last_used_channel.get(guild.id)
-                        if last_channel is not None:
-                            await last_channel.send("Disconnected from the voice channel due to inactivity.")
+                    last_channel = self.last_used_channel.get(guild.id)
+                    if last_channel is not None:
+                        await last_channel.send("Disconnected from the voice channel due to inactivity.")
 
     @check_inactivity.before_loop
     async def before_check_inactivity(self):
@@ -126,6 +124,11 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 async def on_ready():
     print(f"Bot is ready. Logged in as {bot.user}")
     await bot.add_cog(MusicBot(bot))
+
+
+@bot.check
+async def only_allowed_channels(ctx):
+    return ctx.channel.id in ALLOWED_CHANNELS
 
 
 @bot.command(name="stats", help="Shows server stats")
